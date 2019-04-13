f4dd39c4a9841feb4d75486284dc6d96472906fe
VasviDesai <42564047+VasviDesai@users.noreply.github.com>
2019-04-12 20:43:15 -0400

    Analyzing User Text Analysis
    Tokenize
    Sentiment
    Remove punctuations and stop words
    Lemmatize Synonyms

---
+++ UserTextAnalysis.py
@@ -0,0 +1,72 @@
+import nltk
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from textblob import TextBlob
+from nltk.corpus import wordnet
+
+# Function to find polarity
+def sentiment_textblob(feedback): 
+        senti = TextBlob(feedback) 
+        polarity = senti.sentiment.polarity 
+        if -1 <= polarity < -0.5: 
+                label = 'very bad' 
+        elif -0.5 <= polarity < -0.1: 
+                label = 'bad' 
+        elif -0.1 <= polarity < 0.1: 
+                label = 'ok' 
+        elif 0.1 <= polarity < 0.5: 
+                label = 'good' 
+        elif 0.5 <= polarity <= 1: 
+                label = 'positive' 
+        return (polarity, label) 
+
+# Function to find synonyms
+def find_synonyms(x):
+    synonyms = [] 
+    for syn in wordnet.synsets(x): 
+        for l in syn.lemmas(): 
+            synonyms.append(l.name()) 
+    
+    print("Synonyms of " + x + " :",set(synonyms)) 
+
+
+str_input = input("User Text Message: ")
+sentiment = sentiment_textblob(str_input)
+print(sentiment)
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+text = ""
+for char in str_input:
+   if char not in punctuations:
+       text = text + char.lower()
+
+# removing stopwords and word tokenizing
+stop_words=(set(stopwords.words("english"))  - set(["up"])).union(set(["I"]))
+tokenized_word=word_tokenize(text)
+
+filtered_sent=[]
+for w in tokenized_word:
+    if w not in stop_words:
+        filtered_sent.append(w)
+
+lem = WordNetLemmatizer()
+
+print("Tokenized Sentence:",tokenized_word)
+print("Filtered Sentence:",filtered_sent)
+
+# Lemmatize
+lemm_sent = []
+for word in filtered_sent:
+    lemm_sent.append(lem.lemmatize(word,"v"))
+
+print("Lemmatized Sentence:",lemm_sent)
+
+# Finding Synonyms of the lemmatized sentence
+for word in lemm_sent:
+    find_synonyms(word)
+ 
+
+  
+
